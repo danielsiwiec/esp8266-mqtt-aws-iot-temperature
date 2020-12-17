0a492d3080e1cac6d5792fe527fea0606a41eca5
Dan Siwiec <daniel.siwiec@gmail.com>
2020-12-17 14:39:34 -0800

    Fix argument passing

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ from wifi import connect_to_wifi
 f = open('props.json')
 c = ujson.loads(f.read())
 
-connect_to_wifi(c['client_id'], c['wifi_sid'], c['wifi_pass'], c['iot_topic'])
+connect_to_wifi(c['wifi_sid'], c['wifi_pass'])
 
 
 async def send_temp(thermistor, mqtt):
@@ -19,13 +19,13 @@ async def send_temp(thermistor, mqtt):
     while True:
         temp = thermistor.get_temp()
         mqtt.send({'temp': temp})
-        rtc.alarm(rtc.ALARM0, 60000)
+        rtc.alarm(rtc.ALARM0, 5*60000)
         machine.deepsleep()
 
 
 async def main():
     thermistor = Thermistor()
-    mqtt = MQTT(c['iot_endpoint'])
+    mqtt = MQTT(c['client_id'], c['iot_endpoint'], c['iot_topic'])
     uasyncio.create_task(send_temp(thermistor, mqtt))
 
 loop = uasyncio.get_event_loop()
