b8a3b098b75bfc3b4c05c6f2473de90484591410
Dan Siwiec <daniel.siwiec@gmail.com>
2020-12-21 20:51:39 -0800

    Move lines around

--- app/main.py
+++ app/main.py
@@ -6,9 +6,10 @@ from sleep import deep_sleep_seconds
 
 
 props = load_props()
-connect_to_wifi(props['wifi_sid'], props['wifi_pass'])
-thermistor = Thermistor()
 mqtt = MQTT(props['client_id'], props['iot_endpoint'], props['iot_topic'])
+thermistor = Thermistor()
+
+connect_to_wifi(props['wifi_sid'], props['wifi_pass'])
 
 while True:
     temp = thermistor.get_temp()
