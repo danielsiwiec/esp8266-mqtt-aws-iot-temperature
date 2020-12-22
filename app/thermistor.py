7dcf0c14950ea333a76f45839746b981fbd0871e
Dan Siwiec <daniel.siwiec@gmail.com>
2020-12-22 14:58:48 -0800

    Refactor temperature code

--- app/thermistor.py
+++ app/thermistor.py
@@ -10,18 +10,24 @@ class Thermistor:
         time.sleep_ms(100)
         return self.analog.read()
 
-    def get_analog(self):
+    def _get_analog(self):
         sample_size = 5
         return sum([self._get_analog_with_sleep(100) for _ in range(sample_size)])/sample_size
 
     def _steinhart_temperature_C(self, r, Ro=10000.0, To=25.0, beta=3950.0):
-      steinhart = math.log(r / Ro) / beta      # log(R/Ro) / beta
-      steinhart += 1.0 / (To + 273.15)         # log(R/Ro) / beta + 1/To
-      steinhart = (1.0 / steinhart) - 273.15   # Invert, convert to C
-      return steinhart
+        steinhart = math.log(r / Ro) / beta      # log(R/Ro) / beta
+        steinhart += 1.0 / (To + 273.15)         # log(R/Ro) / beta + 1/To
+        steinhart = (1.0 / steinhart) - 273.15   # Invert, convert to C
+        return steinhart
+    
+    def _get_thermistor_resistance(self, Rnom = 10000, adc_resolution=1023):
+        return Rnom / (adc_resolution/self._get_analog() - 1)
+
+    def _c_to_f(self, c):
+        return c * 9/5 + 32
 
     def get_temp(self):
-        R = 10000 / (1023/self.get_analog() - 1)
-        temp_c = self._steinhart_temperature_C(R)
-        temp_f = temp_c * 9/5 + 32
+        Rt = self._get_thermistor_resistance()
+        temp_c = self._steinhart_temperature_C(Rt)
+        temp_f = self._c_to_f(temp_c)
         return temp_f
