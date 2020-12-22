00233136413c97fe79ff2a92f1fb806ed79e0ab9
Dan Siwiec <daniel.siwiec@gmail.com>
2020-12-21 18:13:10 -0800

    Add sleep print out

--- app/sleep.py
+++ app/sleep.py
@@ -4,4 +4,5 @@ def deep_sleep_seconds(seconds):
     rtc = machine.RTC()
     rtc.irq(trigger=rtc.ALARM0, wake=machine.DEEPSLEEP)
     rtc.alarm(rtc.ALARM0, seconds * 1000)
+    print('Entering deep sleep for %s seconds' % seconds)
     machine.deepsleep()
